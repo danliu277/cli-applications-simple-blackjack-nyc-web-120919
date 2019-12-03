c7e41943f6435ce81ee04536f2f0b9d5b0f89a6e
Daniel Liu <danliu277@gmail.com>
2019-12-03 22:02:11 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -38,12 +38,12 @@ end
 def hit?
   # code hit? here
   prompt_user()
-  # input = get_user_input()
-  # if input == "h"
-  #   deal_card()
-  # else
-  #   invalid_command()
-  # end
+  input = get_user_input()
+  if input == "h"
+    deal_card()
+  else
+    invalid_command()
+  end
 end
 
 def invalid_command
